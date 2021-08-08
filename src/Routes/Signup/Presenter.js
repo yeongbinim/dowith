7a125d35ae86d6ed3a8c91b4eb565bb9a218921a
hyunjoebrother <hyunjoe31@gmail.com>
2021-08-08 13:38:13 +0900

    feat: implement Signup page

--- src/Routes/Signup/Presenter.js
+++ src/Routes/Signup/Presenter.js
@@ -1,13 +1,86 @@
 import React from "react";
 import styled from "styled-components";
 
+import Textarea from "Components/Textarea";
+import Button from "Components/Button";
+
+const H3 = styled.h2`
+  width: 90%;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin: 0 auto;
+  margin-bottom: 3rem;
+  border-bottom: 0.1rem solid #c2c2c2;
+  text-align: center;
+  padding: 4rem 0 1.5rem;
+`;
+
+const H2 = styled.h2`
+  font-size: 1.7rem;
+  line-height: 1.3em;
+  font-weight: 600;
+  line-height: 2.8rem;
+`;
+
+const Strong = styled.span`
+  font-weight: 700;
+`;
+
+const H4 = styled.h4`
+  font-size: 1.2rem;
+  line-height: 1.3em;
+  font-weight: 500;
+  line-height: 2.8rem;
+`;
+
+const Link = styled.span`
+  border-bottom: 0.1rem solid #3c68fa;
+  color: "#3c68fa";
+`;
+
+const Background = styled.div`
+  background-color: white;
+  width: 100%;
+  position: absolute;
+  height: 20rem;
+  left: 0;
+  top: 0;
+  z-index: -1;
+`;
+
 const Container = styled.div`
-	padding:1rem;
+  padding: 3rem;
 `;
 
 const Presenter = () => (
-	<Container>
-	</Container>
+  <>
+    <Background />
+    <H3>회원가입</H3>
+    <Container>
+      <H2>
+        <Strong>가입 정보 입력</Strong>{" "}
+      </H2>
+      <H4>두윗과 함께 달릴 준비되셨나요?</H4>
+
+      <br />
+      <br />
+
+      <br />
+      <Textarea />
+      <br />
+      <Textarea />
+      <br />
+      <Textarea />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button />
+    </Container>
+  </>
 );
 
 // Presenter.propTypes = {
