1a7962ba30214ebc2a8e3d8699cb3cfb2cb95d01
hyunjoebrother <hyunjoe31@gmail.com>
2021-08-13 15:39:40 +0900

    feat: Sign page custom + @

--- src/Routes/Kakao.js
+++ src/Routes/Kakao.js
@@ -1,24 +1,80 @@
-import React from 'react';
-
+import React from "react";
+import styled from "styled-components";
+import Logo from "assets/icon-logo.png";
+import Kakao from "assets/kakao_login_medium_narrow.png";
 
 // const CLIENT_ID = "794a04747a9ab3b3b2ca7a39c40c6c0f";
 const CLIENT_ID = "2589e2db71042b3e03d86f96e07aec79";
-const REDIRECT_URI =  "http://127.0.0.1:3000/account/kakao";
+const REDIRECT_URI = "http://127.0.0.1:3000/account/kakao";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-const login = async() => {
-	window.location.replace(KAKAO_AUTH_URL);
-	// const response = await signIn.onLogin();
-	// console.log(response);
-	// alert(response);
-	// window.location.href = "/";
+const login = async () => {
+  window.location.replace(KAKAO_AUTH_URL);
+  // const response = await signIn.onLogin();
+  // console.log(response);
+  // alert(response);
+  // window.location.href = "/";
 };
-const KakaoLogin = () => (
-	<>
-	<br/><br/>
-	<div onClick={()=>login()}>
-		<span>카카오계정 로그인</span>
-	</div></>
-);
 
+const Container = styled.div`
+  padding: 2rem;
+  top: 0;
+  height: 100%;
+  position: relative;
+  margin: auto;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: #4966ff;
+  top: 6rem;
+  left: 0;
+  z-index: -1;
+`;
+
+const Section = styled.section`
+  position: relative;
+  margin: auto;
+  text-align: center;
+  z-index: 1;
+  & > ul {
+    margin-bottom: 5rem;
+  }
+`;
+
+const LogoSection = styled.div`
+  top: 3rem;
+  position: relative;
+  margin: auto;
+`;
+
+const KakaoSection = styled.div`
+  width: 100%;
+  margin-top: 16rem;
+  position: relative;
+  cursor: pointer;
+`;
+
+const KakaoLogin = () => (
+  <>
+    <Container>
+      <Background>
+        <Section>
+          <LogoSection>
+            <div>
+              <img src={Logo} width="60%" height="60%" alt="Logo" />
+            </div>
+          </LogoSection>
+          <KakaoSection>
+            <div onClick={() => login()}>
+              <img src={Kakao} width="45%" height="45%" alt="Kakao" />
+            </div>
+          </KakaoSection>
+        </Section>
+      </Background>
+    </Container>
+  </>
+);
 
 export default KakaoLogin;
--- src/Routes/Main/Introduce/Presenter.js
+++ src/Routes/Main/Introduce/Presenter.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Helmet from "react-helmet";
 import IllustSection from "Components/Main/IllustSection";
 import ButtonLink from "Components/LinkButton";
+import Image0 from "assets/icon-introduce0.png";
 import { ReactComponent as Image1 } from "assets/icon-introduce1.svg";
 import { ReactComponent as Image2 } from "assets/icon-introduce2.svg";
 import { ReactComponent as Icon1 } from "assets/icon-btn1.svg";
@@ -21,7 +22,7 @@ const H2 = styled.h2`
     font-weight: 700;
   }
   margin-top: 2rem;
-  margin-bottom: 1rem;
+  margin-bottom: 2rem;
 `;
 
 const Container = styled.div`
@@ -34,6 +35,11 @@ const Section = styled.section`
   & > ul {
     margin-bottom: 5rem;
   }
+`;
+
+const SectionBox = styled.section`
+  border-bottom: 2px solid #f0f0f0;
+  margin-bottom: 3rem;
 `;
 
 const Centerspan = styled.div`
@@ -57,73 +63,89 @@ const Presenter = () => (
     <IllustSection reverse={false} create={true} contents={[]} />
     <Container>
       <Section>
-        <H2>두윗(Do With)은 어떤 서비스인가요?</H2>
+        <SectionBox>
+          <H2>두윗(Do With)은 어떤 서비스인가요?</H2>
 
-        <Centerspan>
-          우리 삶 속에 일어나는 크고 작은 챌린지들을 응원하는 서비스에요.
-          <br />
-          두윗과 함께라면 목표를 향해 힘껏 달리며 목표를 성취하는 기쁨을 누릴 수
-          있어요!
-          <br />
-          두윗에서는 목표를 향한 레이스를 혼자가 아니라 함께한다는 점이 매우
-          중요한데요.
-          <br />
-          두윗에서는 크고 작은 챌린지들을 함께 할 수 있는 많은 두윗러들을 만날
-          수 있답니다!
-          <br />
-        </Centerspan>
+          <Centerspan>
+            우리 삶 속에 일어나는 크고 작은 챌린지들을 응원하는 서비스에요.
+            <br />
+            두윗과 함께라면 목표를 향해 힘껏 달리며 목표를 성취하는 기쁨을 누릴
+            수 있어요!
+            <br />
+            두윗에서는 목표를 향한 레이스를 혼자가 아니라 함께한다는 점이 매우
+            중요한데요.
+            <br />
+            두윗에서는 크고 작은 챌린지들을 함께 할 수 있는 많은 두윗러들을 만날
+            수 있답니다!
+            <br />
+          </Centerspan>
 
-        <Centerspan>
-          나를 위한 변화뿐만 아니라, 지구와 더 나은 미래를 생각하는 챌린지까지!{" "}
+          <Centerspan>
+            나를 위한 변화뿐만 아니라, 지구와 더 나은 미래를 생각하는
+            챌린지까지! <br />
+            다양한 챌린지들을 두윗러들과 함께 달려보세요!
+          </Centerspan>
           <br />
-          다양한 챌린지들을 두윗러들과 함께 달려보세요!
-        </Centerspan>
-        <br />
-        <br />
-        <Image1 width="50rem" height="16rem" z-index="-1" />
-        <br />
-        <br />
-        <H2>챌린지는 어떻게 이루어지나요?</H2>
+          <br />
 
-        <Image2 width="100%" height="16rem" z-index="-1" />
-        {/* <Image3 width="100%" height="100%" display="inline" z-index="-1" /> */}
+          <Image1 width="50rem" height="16rem" z-index="-1" />
+          <br />
+          <br />
+        </SectionBox>
+        <SectionBox>
+          <H2>챌린지는 어떻게 이루어지나요?</H2>
 
-        <Centerspan>
-          챌린지는 두윗에서 준비한 챌린지 이외에도 원하는 챌린지를 직접 만들 수
-          있어요! <br />
-          두윗러들과 함께 목표 달성을 위해 달려보세요!
-        </Centerspan>
-        <br />
-        <br />
-
-        <H2>두윗의 챌린지 상금과 벌금제도가 궁금해요!</H2>
-        <Centerspan>
-          우리 삶 속에 일어나는 크고 작은 챌린지들을 응원하는 서비스에요.
-          <br />
-          두윗과 함께라면 목표를 향해 힘껏 달리며 목표를 성취하는 기쁨을 누릴 수
-          있어요!
-          <br />
-          두윗에서는 목표를 향한 레이스를 혼자가 아니라 함께한다는 점이 매우
-          중요한데요.
-          <br />
-          두윗에서는 크고 작은 챌린지들을 함께 할 수 있는 많은 두윗러들을 만날
-          수 있답니다!
-          <br />
-        </Centerspan>
-
-        <Button>
-          <ButtonLink
-            url="/"
-            contents={[<Icon1 />, "메인 화면으로 돌아가기"]}
+          <img
+            src={Image0}
+            width="100%"
+            height="30%"
+            max-height="200%"
+            alt="Image0"
           />
-          <ButtonLink
-            url="/challenge"
-            contents={[<Icon2 />, "전체 챌린지 둘러보기"]}
-          />
-          <ButtonLink
-            url="/createchallenge"
-            contents={[<Icon3 />, "챌린지 만들러가기"]}/>
-        </Button>
+          <br />
+          <br />
+          {/* <Image2 width="100%" height="16rem" z-index="-1" /> */}
+          {/* <Image3 width="100%" height="100%" display="inline" z-index="-1" /> */}
+
+          <Centerspan>
+            챌린지는 두윗에서 준비한 챌린지 이외에도 원하는 챌린지를 직접 만들
+            수 있어요! <br />
+            두윗러들과 함께 목표 달성을 위해 달려보세요!
+          </Centerspan>
+          <br />
+          <br />
+        </SectionBox>
+        <SectionBox>
+          <H2>두윗의 챌린지 상금과 벌금제도가 궁금해요!</H2>
+          <Centerspan>
+            우리 삶 속에 일어나는 크고 작은 챌린지들을 응원하는 서비스에요.
+            <br />
+            두윗과 함께라면 목표를 향해 힘껏 달리며 목표를 성취하는 기쁨을 누릴
+            수 있어요!
+            <br />
+            두윗에서는 목표를 향한 레이스를 혼자가 아니라 함께한다는 점이 매우
+            중요한데요.
+            <br />
+            두윗에서는 크고 작은 챌린지들을 함께 할 수 있는 많은 두윗러들을 만날
+            수 있답니다!
+            <br />
+          </Centerspan>
+
+          <Button>
+            <ButtonLink
+              url="/"
+              contents={[<Icon1 />, "메인 화면으로 돌아가기"]}
+            />
+            <ButtonLink
+              url="/challenge"
+              contents={[<Icon2 />, "전체 챌린지 둘러보기"]}
+            />
+            <ButtonLink
+              url="/createchallenge"
+              contents={[<Icon3 />, "챌린지 만들러가기"]}
+            />
+          </Button>
+        </SectionBox>
       </Section>
     </Container>
   </>
